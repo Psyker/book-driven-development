25dcb6a230402820d160183fc8729e639c8b0359
Psyker <bourgoin.theo1@gmail.com>
2020-03-05 14:00:24 +0100

    Add header, subscriber to inject conferences globally

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -25,16 +25,17 @@ class ConferenceController extends AbstractController
 	 */
     public function index(ConferenceRepository $conferenceRepository): Response
 	{
-    	return new Response($this->twig->render('conference/index.html.twig', [
-    		'conferences' => $conferenceRepository->findAll()
-		]));
+    	return new Response($this->twig->render('conference/index.html.twig'));
     }
 	
 	/**
 	 * @Route("/conference/{id}", name="conference")
 	 */
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
-	{
+    public function show(
+    	Request $request,
+		Conference $conference,
+		CommentRepository $commentRepository
+	): Response {
 		$offset = max(0, $request->query->getInt('offset', 0));
 		$paginator = $commentRepository->getCommentPaginator($conference, $offset);
 		
